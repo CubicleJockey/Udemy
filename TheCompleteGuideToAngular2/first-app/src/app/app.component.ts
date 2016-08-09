619b9dbd157d5bd02aa547549d2a706a76998b1c
André Davis <CubicleJockey@users.noreply.github.com>
2016-08-09 11:03:37 -0700

    Additional Components

--- TheCompleteGuideToAngular2/first-app/src/app/app.component.ts
+++ TheCompleteGuideToAngular2/first-app/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
+import { OtherComponent} from './other';
+//import { OtherComponent} from './other/component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   template: `
 	<h1>Inline Template</h1>
-	<p>Another line</p>
+	<fa-other></fa-other>
   `,
   styles: [`
 	h1{
 		color: red;
 	}
-  `]
+  `],
+  directives: [OtherComponent] //<----- After import
 })
 export class AppComponent {
-
+	
 }
