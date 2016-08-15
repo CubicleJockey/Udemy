317287d379bfcb310a2801568cc15b1562ece6c7
André Davis <cubicle.jockey@live.com>
2016-08-15 15:10:38 -0700

    Starting RecipeList and Item components.

--- TheCompleteGuideToAngular2/recipe-book/e2e/app.e2e-spec.ts
+++ TheCompleteGuideToAngular2/recipe-book/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { RecipeBookPage } from './app.po';
 
-describe('recipe-book App', function() {
+describe('dummyRecipe-book App', function() {
   let page: RecipeBookPage;
 
   beforeEach(() => {
