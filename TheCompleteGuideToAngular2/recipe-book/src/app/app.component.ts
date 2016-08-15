1ebc59f8fbb93c67dd4e378d4de54cf235fc59fb
André Davis <cubicle.jockey@live.com>
2016-08-15 13:43:11 -0700

    Skeleton for Recipe Book App

---
+++ TheCompleteGuideToAngular2/recipe-book/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+
+}
