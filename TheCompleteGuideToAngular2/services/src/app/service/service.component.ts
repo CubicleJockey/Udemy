5d0672a7f18b836b8a8f79b3e5355aa07ffa40f2
André Davis <cubicle.jockey@live.com>
2016-08-23 12:56:02 -0700

    Trying out a life service.

--- TheCompleteGuideToAngular2/services/src/app/service/service.component.ts
+++ TheCompleteGuideToAngular2/services/src/app/service/service.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
-
+import { DataService } from './data.service';
 import { CmpAComponent } from './cmp-a.component';
 import { CmpBComponent } from './cmp-b.component';
+import { PatientDetailsComponent } from './patient-details.component';
 
 @Component({
     moduleId: module.id,
     selector: 'si-service',
-    template: `
+    /*template: `
     <si-cmp-a></si-cmp-a>
     <si-cmp-b></si-cmp-b>
-  `,
-    directives: [CmpAComponent, CmpBComponent]
+  `,*/
+    template: `
+      <app-patient-details></app-patient-details>
+     `,
+  directives: [CmpAComponent, CmpBComponent, PatientDetailsComponent],
+  providers: [DataService]
 })
 export class ServiceComponent {
 }
--- TheCompleteGuideToAngular2/services/src/main.ts
+++ TheCompleteGuideToAngular2/services/src/main.ts
@@ -1,10 +1,11 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { ServicesDiAppComponent, environment } from './app/';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(ServicesDiAppComponent);
+bootstrap(ServicesDiAppComponent, [HTTP_PROVIDERS]);
 
