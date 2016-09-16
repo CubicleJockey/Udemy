103bb954281273cbdd6753a12aa95301fac0803e
André Davis <CubicleJockey@users.noreply.github.com>
2016-09-16 13:01:50 -0700

    Url Params and Subscription Cleanup

--- TheCompleteGuideToAngular2/routing/src/app/user/user.component.ts
+++ TheCompleteGuideToAngular2/routing/src/app/user/user.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'dre-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent{
+export class UserComponent implements OnDestroy{
+    private id: string;
+    private paramsSubscription: Subscription;
 
-  constructor(private router: Router) {
+    constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+        /*
+            SnapShot
+            ---------
+            Angular does not reload components if it doesn't need to. So the constructor
+            is not called again and you never update the id.
+        */
+        //this.id = this.activatedRoute.snapshot.params['id'];
 
-  }
+        //Listen to the observable
+        //Creating a subscribe listen will cause a memory link if only
+        //done in the constructor
+        this.paramsSubscription = activatedRoute.params.subscribe(
+            (params: any) => {this.id = params['id']}
+        );
+    }
 
-  public goHome(){
+    public goHome(){
       this.router.navigate(['/']);
-  }
+    }
+
+    public ngOnDestroy(){
+        this.paramsSubscription.unsubscribe();
+    }
 }
